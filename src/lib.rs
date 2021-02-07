342f4aa2c6a83ff6e0928c95a118976eb86602e6
zhoufan <1247714429@qq.com>
2021-02-07 15:36:52 +0800

    add add test

--- src/lib.rs
+++ src/lib.rs
@@ -19,11 +19,11 @@ pub fn run(filename: &str) {
                 Code::dest(&parser.dest()),
                 Code::jump(&parser.jump())
             );
-            bits.push_str("\n");
+            bits.push_str("\r\n");
             hack_file.write_all(bits.as_bytes()).unwrap();
         } else if parser.command_type() == CommandType::ACommand {
             let mut bits = parser.symbol();
-            bits.push_str("\n");
+            bits.push_str("\r\n");
             hack_file.write_all(bits.as_bytes()).unwrap();
         }
     }
@@ -37,4 +37,44 @@ fn get_hack_filename(filename: &str) -> String {
         }
     };
     format!("{}.hack", filename)
+}
+
+mod tests {
+    use super::run;
+    use std::fs::File;
+    use std::io::BufReader;
+    use std::io::prelude::*;
+
+    fn compare_two_files(one: &str, other: &str) -> bool {
+        let f = File::open(one).unwrap();
+        let mut one_reader = BufReader::new(f);
+        let f = File::open(other).unwrap();
+        let mut other_reader = BufReader::new(f);
+        let mut one_line = String::new();
+        let mut other_line = String::new();
+        let mut equal = true;
+        loop {
+            one_line.clear();
+            let one_size = one_reader.read_line(&mut one_line).unwrap();
+            other_line.clear();
+            let other_size = other_reader.read_line(&mut other_line).unwrap();
+            if one_size != other_size || one_line != other_line {
+                equal = false;
+                println!("{} and {} are not equal", one_line, other_line);
+                break
+            }
+
+            // eof
+            if one_size == 0 {
+                break
+            }
+        }
+        equal
+    }
+
+    #[test]
+    fn test_add() {
+        run("Add.asm");
+        assert!(compare_two_files("Add.cmp", "Add.hack"));
+    }
 }
