39df917224653be04de78f698263c4d6a04e4ecd
zhoufan <1247714429@qq.com>
2021-02-24 14:54:15 +0800

    update

--- src/compiler/compilation_engine.rs
+++ src/compiler/compilation_engine.rs
@@ -1,5 +1,5 @@
-use super::tkzr::{TokenType, Tokenizer};
-use std::fs::File;
+use super::tkzr::{TokenType, Tokenizer, KeyWorld};
+use std::{convert::TryInto, fs::File};
 use std::path::Path;
 use xml::writer::{EmitterConfig, EventWriter, XmlEvent};
 
@@ -28,25 +28,7 @@ impl CompilationEngine {
         self.writer.write(start_event).unwrap();
         while self.tkzr.has_more_commands() {
             self.tkzr.advance();
-            match self.tkzr.token_type() {
-                TokenType::KeyWorld(key_world) => {
-                    let key_world = format!("{}", key_world);
-                    self.compile_key_world(&key_world);
-                }
-                TokenType::Symbol(symbol) => {
-                    self.compile_symbol(&symbol);
-                }
-                TokenType::IntConst(val) => {
-                    let val = val.to_string();
-                    self.compile_int(&val);
-                }
-                TokenType::StringConst(val) => {
-                    self.compile_string(&val);
-                }
-                TokenType::Identifier(identifier) => {
-                    self.compile_identifier(&identifier);
-                }
-            };
+            self.compile_current_token();
         }
         let end_event: XmlEvent = XmlEvent::end_element().into();
         self.writer.write(end_event).unwrap();
@@ -67,33 +49,101 @@ impl CompilationEngine {
         self.writer.write(event).unwrap();
     }
 
-    fn compile_key_world(&mut self, key_world: &str) {
-        self.write_start_event("keyword");
-        self.write_characters(key_world);
+    fn compile_class_var_dec(&mut self) {
+        self.write_start_event("classVarDec");
+        self.compile_key_world(self.tkzr.token_type());
+        self.tkzr.advance();
+        loop {
+            self.compile_current_token();
+            if let TokenType::Symbol(s) = self.tkzr.token_type() {
+                if s == ";" {
+                    break;
+                }
+            }
+            self.tkzr.advance();
+        }
         self.write_end_event();
     }
 
-    fn compile_symbol(&mut self, symbol: &str) {
-        self.write_start_event("symbol");
-        self.write_characters(symbol);
-        self.write_end_event();
+    fn compile_subroutine_dec(&mut self) {
+        self.write_start_event("subroutineDec");
+        loop {
+
+        }
     }
 
-    fn compile_int(&mut self, val: &str) {
-        self.write_start_event("integerConstant");
-        self.write_characters(val);
-        self.write_end_event();
+    fn compile_current_token(&mut self) {
+        match self.tkzr.token_type() {
+            TokenType::KeyWorld(key_world) => {
+                if key_world == KeyWorld::Static {
+                    self.compile_class_var_dec();
+                } else {
+                    self.compile_key_world(self.tkzr.token_type());
+                }
+            }
+            TokenType::Symbol(_) => {
+                self.compile_symbol(self.tkzr.token_type());
+            }
+            TokenType::IntConst(_) => {
+                self.compile_int(self.tkzr.token_type());
+            }
+            TokenType::StringConst(_) => {
+                self.compile_string(self.tkzr.token_type());
+            }
+            TokenType::Identifier(_) => {
+                self.compile_identifier(self.tkzr.token_type());
+            }
+        };
+}
+
+    fn compile_key_world(&mut self, token: TokenType) {
+        if let TokenType::KeyWorld(key_world) = token {
+            self.write_start_event("keyword");
+            let key_world = format!("{}", key_world);
+            self.write_characters(&key_world);
+            self.write_end_event();
+        } else {
+            panic!("{:?} is not a KeyWorld", token);
+        }
     }
 
-    fn compile_string(&mut self, val: &str) {
-        self.write_start_event("stringConstant");
-        self.write_characters(val);
-        self.write_end_event();
+    fn compile_symbol(&mut self, token: TokenType) {
+        if let TokenType::Symbol(symbol) = token {
+            self.write_start_event("symbol");
+            self.write_characters(&symbol);
+            self.write_end_event();
+        } else {
+            panic!("{:?} is not a Symbol", token);
+        }
     }
 
-    fn compile_identifier(&mut self, identifier: &str) {
-        self.write_start_event("identifier");
-        self.write_characters(identifier);
-        self.write_end_event();
+    fn compile_int(&mut self, token: TokenType) {
+        if let TokenType::IntConst(val) = token {
+            self.write_start_event("integerConstant");
+            self.write_characters(&val.to_string());
+            self.write_end_event();
+        } else {
+            panic!("{:?} is not a IntConst", token);
+        }
+    }
+
+    fn compile_string(&mut self, token: TokenType) {
+        if let TokenType::StringConst(val) = token {
+            self.write_start_event("stringConstant");
+            self.write_characters(&val);
+            self.write_end_event();
+        } else {
+            panic!("{:?} is not a StringConst");
+        }
+    }
+
+    fn compile_identifier(&mut self, token: TokenType) {
+        if let TokenType::Identifier(identifier) = token {
+            self.write_start_event("identifier");
+            self.write_characters(&identifier);
+            self.write_end_event();
+        } else {
+            panic!("{:?} is not a Identifier");
+        }
     }
 }
